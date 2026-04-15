1ca560e336a4fd12ee5575e0c7bd7ad6f61e4f34
isalvama <inesalvans@gmail.com>
2026-04-15 16:46:30 +0200

    refactor: add null validator to saveObject method in MySql class

--- src/main/java/D/db/MySql.java
+++ src/main/java/D/db/MySql.java
@@ -1,9 +1,11 @@
 package D.db;
 
-public class MySql implements DataBase {
+import java.util.Objects;
 
+public class MySql implements DataBase {
     @Override
     public void saveObject(Object object) {
+        Objects.requireNonNull(object, "the object being saved can not be null");
         System.out.println("Saving " + object.getClass().getName() + " in SQL database ok...");
     }
 }
